63a29974738b9f8220a7f32b9115917574d746ab
HackerSL <151751595+SLhackers@users.noreply.github.com>
2023-11-23 01:54:08 +0530

    BlackTiger.py

--- BlackTiger.py
+++ BlackTiger.py
@@ -154,7 +154,7 @@ def moch_yayan():
                  elif 'friends' in x:
                         continue
                  else:
-                        id.append(x[2] + '|' + x[0].split('/')[1].split('?')[0])
+                        id.append(x[1] + '|' + x[0].split('/')[1].split('?')[0])
                  print('\r\033[1;97m [\033[1;94m•\033[1;97m] \033[1;96m' + str(len(id)) + " \033[1;97mProcess Of Retrieving ID... ",end="")
              if 'Lihat Teman Lain' in str(raw):
                  getid(mbasic.format(parser(raw,'html.parser').find('a',string='Lihat Teman Lain')['href']))
